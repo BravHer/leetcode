baa7d0012d3cac0641d8fec93d24cf975f663c34
BravHer <37463176+BravHer@users.noreply.github.com>
2019-08-09 21:03:13 +0800

    moredetails
    moredetails

---
+++ 78子集.py
@@ -0,0 +1,30 @@
+'''
+给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+
+说明：解集不能包含重复的子集。
+
+示例:
+
+输入: nums = [1,2,3]
+输出:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+'''
+class Solution:
+    def subsets(self, nums):
+        res = []
+        res.append([])
+        for x in nums:
+            new = [s+ [x] for s in res]
+            res = res + new
+        return res
+
