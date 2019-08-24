a04ddc6d4f9824a03c40a4cd6cbed2ad1e4c5cc9
BravHer <37463176+BravHer@users.noreply.github.com>
2019-08-24 16:55:38 +0800

    Add files via upload

--- 2AddTwoNumbers.py
+++ 2AddTwoNumbers.py
@@ -1,9 +1,51 @@
+'''
+给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+示例：
+
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/add-two-numbers
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
 
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l = ListNode(0)
+        sum = 0
+        tmp = l
+        while True:
+            if l1:
+                sum = sum + l1.val
+                l1 = l1.next
+            if l2:
+                sum = sum + l2.val
+                l2 = l2.next
+            tmp.val = sum % 10
+            sum = sum // 10
+            # 结束的条件 两个链表同时为空 并且没有进位（5 + 5 = 10）
+            if not l1 and not l2 and sum == 0:
+                break
+            tmp.next = ListNode(0)
+            tmp = tmp.next
+        return l
+
+'''
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -42,7 +84,7 @@ class Solution(object):
             d.next = ListNode(count)
 
         return l.next
-
+'''
 
 s = Solution()
 l11 = ListNode(3)
